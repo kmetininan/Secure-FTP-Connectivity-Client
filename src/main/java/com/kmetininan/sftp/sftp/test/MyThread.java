75b6f04eccec741b5643e7f226aa24003681bb01
K. Metin INAN <mr.metininan@gmail.com>
2023-09-25 10:05:10 +0300

    refactoring

--- src/main/java/com/kmetininan/sftp/sftp/test/MyThread.java
+++ src/main/java/com/kmetininan/sftp/sftp/test/MyThread.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.ias.sftp.sftp.test;
+package com.kmetininan.sftp.sftp.test;
 
 /**
  *
